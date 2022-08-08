58e99e43c3ce582e1e6dbd6d7e4c585e282f5b73
Benatovich <sambenatovich@gmail.com>
2022-08-07 22:29:58 -0400

    user can now set custom colors for each signal in the custom stoplight

--- src/components/Custom.js
+++ src/components/Custom.js
@@ -1,23 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { EyeDropper } from 'react-eyedrop'
 import colorWheel from '../assets/color-wheel.png'
+import poplightCustom from '../assets/poplight-custom.png'
 
-export default function Playground() {
-    function getColor({ rgb, hex }) {
-        console.log(rgb, hex)
+export default function Custom() {
+  const [topColor, setTopColor] = useState('')
+  const [midColor, setMidColor] = useState('')
+  const [bottomColor, setBottomColor] = useState('')
+  const [signal, setSignal] = useState('top')
+
+    function handleChange(e) {
+      setSignal(e.target.value);
+    }
+
+    function changeColor({ rgb, hex }) {
+      if (signal === 'top') {
+        setTopColor(hex)
+      } else
+      if (signal === 'mid') {
+        setMidColor(hex)
+      } else {
+        setBottomColor(hex)
+      }
     }
 
   return (
-    <div>
-        <h3>Playground</h3>
+    <div className='stoplight'>
         <EyeDropper 
             buttonClasses='eye-dropper'
-            onChange={getColor}
+            onChange={changeColor}
             once={true} >
             EyeDropper
         </EyeDropper>
-        <img src={colorWheel} />
-        {/* <span>{rgb} {hex}</span> */}
+
+        <div className='dropdown'>
+          <select value={signal} onChange={handleChange}>
+            <option value='top'>Top</option>
+            <option value='mid'>Middle</option>
+            <option value='bottom'>Bottom</option>
+          </select>
+        </div>
+
+        <div 
+          className='custom-stoplight'>
+            <img 
+              style={{background: topColor}}
+              className='custom-signal' 
+              src={poplightCustom}/>
+            <img 
+              style={{background: midColor}}
+              className='custom-signal' 
+              src={poplightCustom}/>
+            <img 
+              style={{background: bottomColor}}
+              className='custom-signal' 
+              src={poplightCustom}/>
+        </div>
+
+        <img style={{width: 300}} src={colorWheel} />
     </div>
   )
 }
